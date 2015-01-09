eb6847d284a1c75eae051a4aef98c76b4beab43b
Daniel, Dao Quang Minh <dqminh89@gmail.com>
2015-01-09 00:12:33 -0500

    upgrade execin interface for execdriver
    execin now requires us to pass in the execin's session id so it can create
    cgroups to track orphaned processes
    
    Docker-DCO-1.1-Signed-off-by: Daniel, Dao Quang Minh <dqminh89@gmail.com> (github: dqminh)
    
    Signed-off-by: Daniel, Dao Quang Minh <dqminh89@gmail.com>

--- daemon/execdriver/lxc/driver.go
+++ daemon/execdriver/lxc/driver.go
@@ -521,7 +521,7 @@ func (t *TtyConsole) Close() error {
 	return t.MasterPty.Close()
 }
 
-func (d *driver) Exec(c *execdriver.Command, processConfig *execdriver.ProcessConfig, pipes *execdriver.Pipes, startCallback execdriver.StartCallback) (int, error) {
+func (d *driver) Exec(id string, c *execdriver.Command, processConfig *execdriver.ProcessConfig, pipes *execdriver.Pipes, startCallback execdriver.StartCallback) (int, error) {
 	return -1, ErrExec
 }
 
